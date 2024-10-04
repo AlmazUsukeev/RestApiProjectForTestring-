e4b3cf289f2b111d054a372e85455b8ef8a98d0f
almaz usukeev <usukeev.almazik@mail.ru>
2024-10-04 14:32:08 +0600

    Initial commit

---
+++ src/main/java/com/example/demo/TodoListForTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListForTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListForTestingApplication.class, args);
+
+	}
+}
